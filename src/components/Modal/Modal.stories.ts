6259a3ef1f4052e56f3caa5b991910ef2d051a50
Deryl Feyza <muhammadderylfeyza@gmail.com>
2024-07-29 20:17:46 +0700

    modal (wip #1)

--- src/components/Modal/Modal.stories.ts
+++ src/components/Modal/Modal.stories.ts
@@ -4,49 +4,36 @@ import { Modal } from "./Modal";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-	title: "Example/Modal",
+	title: "Orgasm/Modal",
 	component: Modal,
 	parameters: {
 		// Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
 		layout: "centered",
 	},
 	// This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+	argTypes: {
+		variant: {
+			control: {
+				type: "select",
+				options: ["dark", "light"],
+			},
+		},
+	},
 	tags: ["autodocs"],
 	// More on argTypes: https://storybook.js.org/docs/api/argtypes
-	argTypes: {
-		backgroundColor: { control: "color" },
-	},
 	// Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-	args: { onClick: fn() },
+	args: { variant: "light", onClick: fn() },
 } satisfies Meta<typeof Modal>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary: Story = {
+export const Form: Story = {
 	args: {
-		primary: true,
-		label: "Modal",
-	},
-};
-
-export const Secondary: Story = {
-	args: {
-		label: "Modal",
-	},
-};
-
-export const Large: Story = {
-	args: {
-		size: "large",
-		label: "Modal",
-	},
-};
-
-export const Small: Story = {
-	args: {
-		size: "small",
-		label: "Modal",
+		header: "Gay SEX!!",
+		variant: "dark",
+		positiveLabel: "Success",
+		negativeLabel: "Cancel",
 	},
 };
