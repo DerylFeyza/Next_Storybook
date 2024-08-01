3b6c257bbaefcffc26341843e3c0fb783a523b73
Deryl Feyza <muhammadderylfeyza@gmail.com>
2024-08-01 09:45:01 +0700

    fix: breadcrumb

--- src/components/Breadcrumbs/Breadcrumb.stories.tsx
+++ src/components/Breadcrumbs/Breadcrumb.stories.tsx
@@ -1,62 +1,66 @@
-import React from 'react';
-import { Meta, StoryObj } from '@storybook/react';
-import { Breadcrumbb } from './Breadcrumb';
-import { CalendarMonthFilled, CalendarMonthRegular, bundleIcon } from '@fluentui/react-icons';
+import React from "react";
+import { Meta, StoryObj } from "@storybook/react";
+import { StoryBreadcrumb } from "./Breadcrumb";
+import {
+	CalendarMonthFilled,
+	CalendarMonthRegular,
+	bundleIcon,
+} from "@fluentui/react-icons";
 
 const CalendarMonth = bundleIcon(CalendarMonthFilled, CalendarMonthRegular);
 
-const meta: Meta<typeof Breadcrumbb> = {
-  title: "Components/Breadcrumbs",
-  component: Breadcrumbb,
-  parameters: {
-    layout: "centered",
-  },
-  tags: ["autodocs"],
+const meta: Meta<typeof StoryBreadcrumb> = {
+	title: "Components/Breadcrumbs",
+	component: StoryBreadcrumb,
+	parameters: {
+		layout: "centered",
+	},
+	tags: ["autodocs"],
 };
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {
-    variant: 'medium',
-    data: [
-      {
-        title: 'Home',
-        path: '/home'
-      },
-      {
-        title: 'Overview',
-        path: '/overview'
-      },
-      {
-        title: 'Details',
-        path: '/details'
-      }
-    ],
-  },
+	args: {
+		variant: "medium",
+		data: [
+			{
+				title: "Home",
+				path: "/home",
+			},
+			{
+				title: "Overview",
+				path: "/overview",
+			},
+			{
+				title: "Details",
+				path: "/details",
+			},
+		],
+	},
 };
 
 export const Custom: Story = {
-  args: {
-    variant: 'medium',
-    data: [
-      {
-        title: 'Home',
-        path: '/home',
-        icon: <CalendarMonth />
-      },
-      {
-        title: 'Overview',
-        path: '/overview',
-        icon: <CalendarMonth />
-      },
-      {
-        title: 'Details',
-        path: '/details',
-        icon: <CalendarMonth />
-      }
-    ],
-    separator: '>',
-  },
-}
+	args: {
+		variant: "medium",
+		data: [
+			{
+				title: "Home",
+				path: "/home",
+				icon: <CalendarMonth />,
+			},
+			{
+				title: "Overview",
+				path: "/overview",
+				icon: <CalendarMonth />,
+			},
+			{
+				title: "Details",
+				path: "/details",
+				icon: <CalendarMonth />,
+			},
+		],
+		separator: ">",
+	},
+};
--- src/components/Breadcrumbs/Breadcrumb.tsx
+++ src/components/Breadcrumbs/Breadcrumb.tsx
@@ -1,56 +1,52 @@
-import * as React from "react";
+import React from "react";
 import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbDivider,
-  BreadcrumbButton,
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbDivider,
+	BreadcrumbButton,
 } from "@fluentui/react-components";
-import {
-  CalendarMonthFilled,
-  CalendarMonthRegular,
-  bundleIcon,
-} from "@fluentui/react-icons";
 import { HTMLAttributes } from "react";
 
-const CalendarMonth = bundleIcon(CalendarMonthFilled, CalendarMonthRegular);
-
 export interface BreadcrumbItemData {
-  title: string;
-  path: string;
-  icon?: JSX.Element;
+	title: string;
+	path?: string;
+	icon?: JSX.Element;
 }
 
-export interface BreadcrumbbProps extends HTMLAttributes<HTMLDivElement> {
-  variant?: "large" | "medium" | "small";
-  data: BreadcrumbItemData[];
-  separator?: string;
+export interface StoryBreadcrumbProps extends HTMLAttributes<HTMLDivElement> {
+	variant?: "large" | "medium" | "small";
+	data: BreadcrumbItemData[];
+	separator?: string;
 }
 
-export const Breadcrumbb = ({
-  variant = "medium",
-  data,
-  separator = ">",
-}: BreadcrumbbProps) => {
-  const variants = {
-    large: "text-lg space-x-4 font-semibold",
-    medium: "text-base space-x-4 font-medium",
-    small: "text-sm space-x-2 font-normal",
-  };
+export const StoryBreadcrumb = ({
+	variant = "medium",
+	data,
+	separator = ">",
+	...props
+}: StoryBreadcrumbProps) => {
+	const variants = {
+		large: "text-lg space-x-4 font-semibold",
+		medium: "text-base space-x-4 font-medium",
+		small: "text-sm space-x-2 font-normal",
+	};
 
-  return (
-    <Breadcrumb aria-label="Breadcrumb" className={variants[variant]}>
-      {data.map((item, index) => (
-        <React.Fragment key={index}>
-          <BreadcrumbItem>
-            <BreadcrumbButton href={item.path} icon={item.icon}>
-              {item.title}
-            </BreadcrumbButton>
-          </BreadcrumbItem>
-          {index < data.length - 1 && (
-            <BreadcrumbDivider>{separator}</BreadcrumbDivider>
-          )}
-        </React.Fragment>
-      ))}
-    </Breadcrumb>
-  );
-}
+	return (
+		<Breadcrumb aria-label="Breadcrumb" className={variants[variant]}>
+			{data.map((item, index) => (
+				<React.Fragment key={index}>
+					<BreadcrumbItem>
+						<div {...props}>
+							<BreadcrumbButton href={item.path} icon={item.icon}>
+								{item.title}
+							</BreadcrumbButton>
+						</div>
+					</BreadcrumbItem>
+					{index < data.length - 1 && (
+						<BreadcrumbDivider>{separator}</BreadcrumbDivider>
+					)}
+				</React.Fragment>
+			))}
+		</Breadcrumb>
+	);
+};
